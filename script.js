2cbbdfcc84919a0bef800346c91d7a19ac63b804
Paul_Aroo <timmypaul2006@gmail.com>
2022-01-22 14:05:18 +0100

    add: scroll up button

--- script.js
+++ script.js
@@ -1,12 +1,20 @@
 $(document).ready(function() {
   $(window).scroll(function() {
-    if(this.scrollY > 20) {
+    if(this.scrollY > 20)
       $('.navbar').addClass("sticky");
-    }
-    else {
+    else
       $('.navbar').removeClass("sticky");
-    }
+
+    if(this.scrollY > 2000)
+      $('.scroll-up-btn').addClass("show");
+    else
+      $('.scroll-up-btn').removeClass("show");
   });
+
+  // slide up script
+  $('.scroll-up-btn').click(() => {
+    $('html').animate({scrollTop: 0});
+  })
 
   // toggle menu/navbar script
   $('.menu-btn').click(function() {
